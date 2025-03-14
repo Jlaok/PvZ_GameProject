1e54254c1b78d1b63b3e368fedf678611afafc36
Max-kiernan <C24424254@myTUDublin.ie>
2025-03-14 14:33:19 +0000

    Update main(2).c

--- main(2).c
+++ main(2).c
@@ -204,28 +204,34 @@ int main()
 
 					count++;
 
-					switch(zom_left)
+					switch(zombieDead)
 					{
+						case 0:
+							{
+								printText("zom left: 10", 10, 5, RGBToWord(255,0,0),0);
+								break;
+							}
+
 						case 1:
 							{
-								printText("zom left: 1", 10, 5, RGBToWord(255,0,0),0);
+								printText("zom left: 9", 10, 5, RGBToWord(255,0,0),0);
 								break;
 							}
 						
 						case 2:
 							{
-								printText("zom left: 2", 10, 5, RGBToWord(255,0,0),0);
+								printText("zom left: 8", 10, 5, RGBToWord(255,0,0),0);
 								break;
 							}
 							
 						case 3:
 							{
-								printText("zom left: 3", 10, 5, RGBToWord(255,0,0),0);
+								printText("zom left: 7", 10, 5, RGBToWord(255,0,0),0);
 								break;
 							}
 						case 4:
 							{
-								printText("zom left: 4", 10, 5, RGBToWord(255,0,0),0);
+								printText("zom left: 6", 10, 5, RGBToWord(255,0,0),0);
 								break;
 							}
 						case 5:
@@ -235,30 +241,30 @@ int main()
 							}
 						case 6:
 							{
-								printText("zom left: 6", 10, 5, RGBToWord(255,0,0),0);
+								printText("zom left: 4", 10, 5, RGBToWord(255,0,0),0);
 								break;
 							}
 						case 7:
 							{
-								printText("zom left: 7", 10, 5, RGBToWord(255,0,0),0);
+								printText("zom left: 3", 10, 5, RGBToWord(255,0,0),0);
 								break;
 							}
 						
 						case 8:
 							{
-								printText("zom left: 8", 10, 5, RGBToWord(255,0,0),0);
+								printText("zom left: 2", 10, 5, RGBToWord(255,0,0),0);
 								break;
 							}
 						
 						case 9:
 							{
-								printText("zom left: 9", 10, 5, RGBToWord(255,0,0),0);
+								printText("zom left: 1", 10, 5, RGBToWord(255,0,0),0);
 								break;
 							}
 						
 						case 10:
 							{
-								printText("zom left: 10", 10, 5, RGBToWord(255,0,0),0);
+								printText("zom left: 0", 10, 5, RGBToWord(255,0,0),0);
 								break;
 							}
 						default:
